7803faff4e831cd934bf881536f3fccc2cc98d1e
shriiix <shrighadi003@gmail.com>
2024-06-09 00:56:23 +0530

    cloudinary file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+const uploadOnCloudinary = async(localFilePath)=> {
+    try{
+        if(!localFilePath)return null //wrong file path
+        //upload file on Clodinary 
+        const responce = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file upload succesfully
+        console.log(responce.url, "file uploaded on cloudinary ");
+        return responce;
+
+    }catch(error){
+        fs.unlinkSync(localFilePath) //removed the locally saved files from the server
+        return null;
+
+    }
+
+}
+
+export{uploadOnCloudinary}
+
+
+
+
