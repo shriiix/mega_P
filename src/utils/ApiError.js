7c66fb8cfe44de7d58c025c7f625833e65f518c7
shriiix <shrighadi003@gmail.com>
2024-06-08 20:37:28 +0530

    error fix

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -6,7 +6,7 @@ class ApiError extends Error{
         statusCode,
         message = "Something went Wrong",
         errors = [],
-        statck = ""
+        stack = ""
 
     ){
 
@@ -18,8 +18,8 @@ class ApiError extends Error{
         this.success = false
         this.errors = errors
 
-        if(statck){
-            this.stack = statck 
+        if(stack){
+            this.stack = stack 
         }else{
             Error.captureStackTrace(this,this.costructor)
         }
