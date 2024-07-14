e75d6da4ad61b5da8115bc5b2ae75896389b5610
shriiix <shrighadi003@gmail.com>
2024-07-14 13:34:42 +0530

    logout access token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,7 +51,6 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
-
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
@@ -59,13 +58,38 @@ userSchema.pre("save", async function (next) {
     next()
 })
 
+
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 }
+
 userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
+
 
 export const User = mongoose.model("User", userSchema)
