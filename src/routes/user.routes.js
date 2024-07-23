8ad06815bdd481bdbb88b2586ff1bd30626eb0c3
shriiix <shrighadi003@gmail.com>
2024-07-23 15:53:23 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    registerUser,
+    updateAccountDetail,
+    updateUserAvatar,
+    updateUserCoverImage 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
@@ -22,6 +32,19 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+//patch only one detail is updating.
+//post update everything
+router.route("/update-account").patch(verifyJWT,updateAccountDetail)
+router.route("/avatar-update").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+//BCZ we used params. 
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
 
 export default router
 
